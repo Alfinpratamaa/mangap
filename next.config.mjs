04374100258fb9637e3312b57e5291423c2ca626
muhamad alfin pratama <muhamadalfinpratamaa@gmail.com>
2024-06-25 15:00:09 +0700

    chore: Refactor image domain configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,48 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains:
-      process.env.NEXT_IMAGE_ALLOW_ALL_DOMAINS === "true"
-        ? []
-        : [
-            "res.cloudinary.com",
-            "komikcast.lol",
-            "kiryuu.id",
-            "svr4.imgkc1.my.id",
-            "svr5.imgkc2.my.id",
-            "svr6.imgkc3.my.id",
-          ],
-    remotePatterns:
-      process.env.NEXT_IMAGE_ALLOW_ALL_DOMAINS === "true"
-        ? [
-            {
-              protocol: "https",
-              hostname: "**",
-            },
-          ]
-        : [],
+    domains: [
+      "res.cloudinary.com",
+      "komikcast.lol",
+      "kiryuu.id",
+      "svr4.imgkc1.my.id",
+      "svr5.imgkc2.my.id",
+      "svr6.imgkc3.my.id",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "komikcast.lol",
+      },
+      {
+        protocol: "http",
+        hostname: "kiryuu.id",
+      },
+      {
+        protocol: "http",
+        hostname: "svr4.imgkc1.my.id",
+      },
+      {
+        protocol: "http",
+        hostname: "svr5.imgkc2.my.id",
+      },
+      {
+        protocol: "http",
+        hostname: "svr6.imgkc3.my.id",
+      },
+      {
+        protocol: "https",
+        hostname: "komikcast.moe",
+      },
+    ],
   },
 };
 
