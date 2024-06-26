257aa27dd24fb4727fc4f0f97d779597e9d4b2cb
muhamad alfin pratama <muhamadalfinpratamaa@gmail.com>
2024-06-26 19:56:41 +0700

    add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://mangapp.me",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://mangapp.me/manga-list",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+    {
+      url: "https://mangapp.me/genres",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+  ];
+}
