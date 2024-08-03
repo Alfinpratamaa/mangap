f86cf67891bff1219ca066b11621c58a0ed09b30
muhamad alfin pratama <muhamadalfinpratamaa@gmail.com>
2024-08-03 21:54:52 +0700

    chore: Refactor sitemap to fetch comic hrefs and update sitemap entries

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,25 @@
 import { MetadataRoute } from "next";
+import axios from "axios";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+async function fetchComicHrefs() {
   const totalPages = 300;
+  const comicHrefs: string[] = [];
+
+  for (let page = 1; page <= totalPages; page++) {
+    const response = await axios.get(
+      `https://facebook.69dev.id/daftar-komik/${page}`
+    );
+    const comics = response.data.data.comics;
+    comics.forEach((comic: any) => {
+      comicHrefs.push(comic.href);
+    });
+  }
+
+  return comicHrefs;
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const comicHrefs = await fetchComicHrefs();
 
   const sitemapEntries: MetadataRoute.Sitemap = [
     {
@@ -17,8 +35,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
   ];
-
-  for (let page = 1; page <= totalPages; page++) {
+  comicHrefs.forEach((href) => {
+    sitemapEntries.push({
+      url: `https://mangapp.me/komik${href}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
+    });
+  });
+  for (let page = 1; page <= 300; page++) {
     sitemapEntries.push({
       url: `https://mangapp.me/manga-list/${page}`,
       lastModified: new Date(),
