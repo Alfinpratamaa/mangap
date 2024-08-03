7b7b1cd292584a2739dbd568c55f2f82d2b79c2f
muhamad alfin pratama <muhamadalfinpratamaa@gmail.com>
2024-08-03 21:24:43 +0700

    refactor functionality of robots and sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: ["/"],
+        allow: "/",
       },
     ],
     sitemap: "https://mangapp.me/sitemap.xml",
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,14 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+  const totalPages = 300;
+
+  const sitemapEntries: MetadataRoute.Sitemap = [
     {
       url: "https://mangapp.me",
       lastModified: new Date(),
       changeFrequency: "always",
       priority: 1,
-    },
-    {
-      url: "https://mangapp.me/manga-list",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.7,
     },
     {
       url: "https://mangapp.me/genres",
@@ -21,4 +17,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
   ];
+
+  for (let page = 1; page <= totalPages; page++) {
+    sitemapEntries.push({
+      url: `https://mangapp.me/manga-list/${page}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
+    });
+  }
+
+  return sitemapEntries;
 }
