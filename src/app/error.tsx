346d5a7c39534598893d0b36bb2e453e4b9046f8
muhamad alfin pratama <muhamadalfinpratamaa@gmail.com>
2024-08-04 19:09:26 +0700

    make client page for error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,3 +1,4 @@
+'use client'
 const ErrorPage = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen dark:bg-primary bg-white text-primary dark:text-white">
